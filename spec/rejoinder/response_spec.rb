a8f844b3609efeb6d51403ce3083b448e035e25c
Diogo Osório <diogo.g.osorio@gmail.com>
2017-10-14 11:49:12 +0100

    Add the initial battery of tests for the response class

---
+++ spec/rejoinder/response_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+require 'rejoinder/response'
+require 'rejoinder/response_error'
+
+RSpec.describe Rejoinder::Response do
+  let(:context) { 'response' }
+  let(:error_message) { 'message' }
+  let(:error_code) { 1001 }
+  let(:response_error) {
+    Rejoinder::ResponseError.new(message: error_message, code: error_code)
+  }
+
+  context 'when both an error and context are used' do
+    describe '#initialize' do
+      it do
+        expect { described_class.new(context: context, error: response_error) }
+          .to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  context 'when the response is successful' do
+    subject { described_class.success(context: context) }
+
+    describe '#success?' do
+      it do
+        expect(subject.success?).to be true
+      end
+    end
+
+    describe '#error?' do
+      it do
+        expect(subject.error?).to be false
+      end
+    end
+
+    describe '#error!' do
+      it do
+        expect { subject.error! }.to_not raise_error
+      end
+    end
+
+    describe '#context' do
+      it do
+        expect(subject.context).to eq context
+      end
+    end
+  end
+
+  context 'when the response is not successful' do
+    subject { described_class.error(message: error_message, code: error_code) }
+
+    describe '#success?' do
+      it do
+        expect(subject.success?).to be false
+      end
+    end
+
+    describe '#error?' do
+      it do
+        expect(subject.error?).to be true
+      end
+    end
+
+    describe '#error' do
+      it 'should return the error message' do
+        expect(subject.error.message).to eq error_message
+      end
+
+      it 'should return the error code' do
+        expect(subject.error.code).to eq error_code
+      end
+    end
+
+    describe '#error!' do
+      it do
+        expect { subject.error! }
+          .to raise_error(Rejoinder::ResponseError, error_message)
+      end
+
+      it do
+        expect { subject.error! }.to raise_error do |error|
+          expect(error.code).to eq error_code
+        end
+      end
+    end
+  end
+end
